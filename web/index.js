a713564bd12177cb555e5cd9c68b000569584990
wnwjq462 <vxc13@naver.com>
2021-06-13 19:52:36 +0900

    (backend): set frame & swagger - API Document

---
+++ web/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const path = require("path");
+const swaggerUi = require("swagger-ui-express");
+const yaml = require("yamljs");
+
+dotenv.config();
+const app = express();
+const swaggerSpec = yaml.load(path.join(__dirname, "./swagger/build.yaml"));
+app.set("port", process.env.PORT || 3000);
+
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("main page");
+});
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 이 존재하지 않습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(app.get("port"), () => {
+  console.log("Server is running on ", app.get("port"));
+});
