22aecf6842968f9e71587b498cd10971dafd10ad
JyunKim <kann1997@naver.com>
2021-06-22 21:33:45 +0900

    (backend): Add getDining function without page

--- web/controller/jihyun.js
+++ web/controller/jihyun.js
@@ -1,9 +1,48 @@
 const express = require("express");
+const Dining = require("../models/dining");
+const Keyword = require("../models/keyword");
+
+const getScore = (score) => {
+  if (score == "total") {
+    return "totalScore";
+  } else if (score == "taste") {
+    return "tasteScore";
+  } else if (score == "price") {
+    return "priceScore";
+  } else if (score == "service") {
+    return "serviceScore";
+  } else if (score == "mood") {
+    return "moodScore";
+  }
+};
 
 exports.getDining = (req, res, next) => {
-  res.send("Dining page");
+  const score = getScore(req.query.score);
+  const keyword = req.query.keyword;
+  try {
+    if (keyword) {
+      const dinings = Dining.findAll({
+        include: {
+          model: Keyword,
+          where: { name: keyword },
+        },
+        order: [[score, "DESC"]],
+        limit: 5,
+      });
+      res.json(dinings);
+    } else {
+      const dinings = Dining.findAll({
+        order: [[score, "DESC"]],
+        limit: 5,
+      });
+      res.json(dinings);
+    }
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  }
 };
 
 exports.getUserLike = (req, res, next) => {
-  res.send("User Like page");
+  
 };
