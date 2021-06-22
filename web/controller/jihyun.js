c8871b6b01fab3fa802178f0f2ca28df6ee5941a
JyunKim <kann1997@naver.com>
2021-06-22 21:59:54 +0900

    (backend): Add getUserLike function without page

--- web/controller/jihyun.js
+++ web/controller/jihyun.js
@@ -19,9 +19,10 @@ const getScore = (score) => {
 exports.getDining = (req, res, next) => {
   const score = getScore(req.query.score);
   const keyword = req.query.keyword;
+  const dinings;
   try {
     if (keyword) {
-      const dinings = Dining.findAll({
+      dinings = Dining.findAll({
         include: {
           model: Keyword,
           where: { name: keyword },
@@ -29,20 +30,36 @@ exports.getDining = (req, res, next) => {
         order: [[score, "DESC"]],
         limit: 5,
       });
-      res.json(dinings);
     } else {
-      const dinings = Dining.findAll({
+      dinings = Dining.findAll({
+        include: { model: Keyword },
         order: [[score, "DESC"]],
         limit: 5,
       });
-      res.json(dinings);
     }
-  } catch(error) {
+    res.json(dinings);
+  } catch (error) {
     console.error(error);
     return next(error);
   }
 };
 
 exports.getUserLike = (req, res, next) => {
-  
+  const score = getScore(req.query.score);
+  try {
+    const user = User.findOne({ where: { id: req.user.id } });
+    if (user) {
+      const dinings = user.getDinings({
+        include: { model: Keyword },
+        order: [[score, "DESC"]],
+        limit: 5,
+      });
+      res.json(dinings);
+    } else {
+      res.status(404).send("사용자가 존재하지 않습니다.");
+    }
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
 };
