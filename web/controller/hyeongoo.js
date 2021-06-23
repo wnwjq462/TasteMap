61f89cd68198d2229aaef0c3592a3357eb5928be
hgoolee <hgl9023@naver.com>
2021-06-23 22:10:05 +0900

    (backend): Create postUserLike, deleteUserLike, getUserKeyword functions

--- web/controller/hyeongoo.js
+++ web/controller/hyeongoo.js
@@ -1,13 +1,79 @@
 const express = require("express");
 
-exports.postUserLike = (req, res, next) => {};
+const Dining = require("../models/dining");
+const Keyword = require("../models/keyword");
+const User = require("../models/user");
 
-exports.deleteUserLike = (req, res, next) => {};
+// POST /user/like/{diningId}
+exports.postUserLike = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {id: req.user.id}
+    });
 
-exports.getUserKeyword = (req, res, next) => {
-  res.send("User Keyword Page");
+    if(user) {
+      const dining = await Dining.findOne({
+        where: {dining_id: req.params.dining_id}
+      });
+      await user.addDining(dining);
+    }
+    else {
+      res.status(404).send("User not found!");
+    }
+  } catch(err) {
+    console.error(err);
+    return next(err);
+  }
 };
 
+
+// DELETE /user/like/{diningId}
+exports.deleteUserLike = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {id: req.user.id}
+    });
+
+    if(user) {
+      const dining = await Dining.findOne({
+        where: {dining_id: req.params.dining_id}
+      });
+      await user.removeDinings(dining);
+    }
+    else {
+      res.status(404).send("User not found!");
+    }
+  } catch(err) {
+    console.error(err);
+    return next(err);
+  }
+};
+
+
+// GET /user/keyword
+exports.getUserKeyword = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {id: req.user.id}
+    });
+
+    if(user) {
+      const keywords = await user.getKeywords({
+        attributes: ["name"]
+      });
+      res.status(200).json(keywords);
+    }
+    else {
+      res.status(404).send("User not found!");
+    }
+  } catch(err) {
+    console.error(err);
+    return next(err);
+  }
+};
+
+
+// GET /keyword
 exports.getKeyword = (req, res, next) => {
-  res.send("Keyword Page");
+  res.send("This is Keyword Page");
 };
